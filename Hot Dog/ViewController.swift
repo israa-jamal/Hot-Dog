c777a42fd122e8e072c1ee20e33c887f5eef6ed2
Esraa Gamal <esraa.gamal98@gmail.com>
2020-06-03 00:38:25 +0200

    Analyzing taken photos

--- Hot Dog/ViewController.swift
+++ Hot Dog/ViewController.swift
@@ -24,10 +24,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
         ImageView.image = pickedImage
+            guard let ciImage = CIImage(image: pickedImage) else{
+                fatalError("Taken image can't be converted to CIImage")
+            }
+            detect(image: ciImage)
             
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
+    func detect (image : CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Couldn't load CoreML model")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model couldn't process Image")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
+    }
+    
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
